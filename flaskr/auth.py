05de68df7b3854b39414d9983291fcf9c58ccc23
mac <bruce@MacBook-Pro.local>
2020-11-29 18:39:47 +0800

    auth

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,10 +1,11 @@
 import functools
+import datetime
 from flask import (
-    Blueprint, request, redirect, url_for, flash, render_template
+    Blueprint, request, redirect, url_for, flash, render_template, session, g
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
-from flaskr.forms import RegistrationForm
+from flaskr.forms import RegistrationForm, LoginForm, InformationForm, PWInformationForm
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -14,25 +15,113 @@ def register():
     if request.method == 'POST' and form.validate():
         username = request.form['username']
         password = request.form['password']
-        mobile_number = request.form['mobile_number']
+        email = request.form['email']
         request_ip = request.remote_addr
 
         db = get_db()
         error = None
 
         if db.execute(
-            'SELECT id FROM user WHERE username=? OR mobile_number=?',
-            (username, mobile_number)
+            'SELECT id FROM user WHERE username=? OR email=?',
+            (username, email)
         ).fetchone() is not None:
-            error = 'Username or mobile num is already registered'
+            error = 'Username or email num is already registered'
 
         if error is None:
             db.execute(
-                'INSERT INTO user (username, password, mobile_number, request_ip)'
+                'INSERT INTO user (username, password, email, request_ip)'
                 ' VALUES (?, ?, ?, ?)',
-                (username, generate_password_hash(password), mobile_number, request_ip)
+                (username, generate_password_hash(password), email, request_ip)
             )
             db.commit()
             return redirect(url_for('auth.login'))
+        
         flash(error)
+    
     return render_template('auth/register.html', form=form)
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        user = db.execute(
+            'SELECT * FROM user WHERE username=?',
+            (username,)
+        ).fetchone()
+
+        if user is None:
+            error = '用户名不正确'
+        elif not check_password_hash(user['password'], password):
+            error = '密码不正确'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('auth.update'))
+
+        flash(error)
+
+    return render_template('auth/login.html', form=form)
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))    
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id =session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
+
+@bp.route('/update', methods=('GET', 'POST'))
+@login_required
+def update():
+    if request.form:
+        form = PWInformationForm(request.form)
+        
+        username = request.form['username']
+        sex = request.form['sex']
+        email = request.form['email']
+        birthdate = datetime.datetime.strptime(request.form['birthdate'], "%Y-%m-%d")
+        updated = datetime.datetime.now()
+
+        if 'whether_password' in request.form:
+            validate = form.validate()
+            sql_script = "UPDATE user SET username=?, email=?, sex=?, birthdate=?, updated=?, password=? WHERE id=?"
+            values = (username, email, sex, birthdate, updated, generate_password_hash(request.form['password']), g.user['id'])
+        else:
+            validate = InformationForm(request.form).validate()
+            sql_script = "UPDATE user SET username=?, email=?, sex=?, birthdate=?, updated=? WHERE id=?"
+            values = (username, email, sex, birthdate, updated, g.user['id'])
+
+    else:
+        form = PWInformationForm(data=g.user) 
+    
+    if request.method == 'POST' and validate: 
+        db = get_db()
+        
+        db.execute(
+            sql_script,
+            values 
+        )
+        db.commit()
+        return redirect(url_for('auth.update'))
+    return render_template('auth/update.html', form=form)
--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,24 +1,81 @@
-from flask_wtf import FlaskForm
-from wtforms import Form, StringField, validators, PasswordField
-from wtforms.fields.html5 import TelField
+from wtforms import Form, StringField, validators, PasswordField, DateField, RadioField, BooleanField
+from wtforms.fields.html5 import EmailField
 
 class RegistrationForm(Form):
     username = StringField(
-        label='用户名', 
         validators=[
-            validators.Regexp(regex=r'\w{2,20}', message='2~20字母数字及下划线')], 
+            validators.Regexp(regex=r'\w{2,20}', message='请输入正确用户名')],
+        render_kw={'placeholder':'用户名'},
+        description='2~20字母数字及下划线',
         id='inputUsername')
-    mobile_number = TelField(
-        label='手机', 
-        validators=[validators.Length(min=11, max=11, message='请输入正确的手机号码')], 
-        id='inputTelNum')
+    email = EmailField(
+        validators=[validators.Regexp(regex=r'.+@.+\.com$', message='请输入正确邮箱')],
+        render_kw={'placeholder':'you@example.com'},
+        description='电子邮箱',
+        id='inputEmail')
     password = PasswordField(
-        label='密码', 
         validators=[
-            validators.Regexp(regex=r'^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,20}', message='8~16字母数字'),
+            validators.Regexp(regex=r'^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,20}', message='请输入正确密码'),
             validators.EqualTo('confirm', message='密码不匹配')
         ],
+        render_kw={'placeholder':'密码'},
+        description='8~16字母数字',
+        id='inputPassword')
+    confirm = PasswordField(
+        render_kw={'placeholder':'确认密码'},
+        description='再次输入密码',
+        id='inputConfirm')
+
+class LoginForm(Form):
+    username = StringField(
+        validators=[
+            validators.Regexp(regex=r'\w{2,20}', message='2~20字母数字及下划线')], 
+        render_kw={'placeholder': '用户名'},
+        id='inputUsername')
+    password = PasswordField(
+        validators=[
+            validators.Regexp(regex=r'^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,20}', message='8~16字母数字'),
+        ],
+        render_kw={'placeholder': '密码'},
+        id='inputPassword')
+
+class InformationForm(Form):
+    username = StringField(
+        label='用户名',
+        validators=[
+            validators.Regexp(regex=r'\w{2,20}', message='请输入正确用户名')],
+        render_kw={'placeholder':'Bruce'},
+        description='2~20字母数字及下划线',
+        id='inputUsername')
+    sex = RadioField(
+        label='性别',
+        choices=[
+        ('male', '男'), ('female', '女')],
+        id='inputSex')
+    email = EmailField(
+        label='电子邮箱',
+        validators=[validators.Regexp(regex=r'.+@.+\.com$', message='请输入正确邮箱')],
+        render_kw={'placeholder':'you@example.com'},
+        description='电子邮箱',
+        id='inputEmail')
+    birthdate = DateField(
+        label='生日',
+        render_kw={'placeholder': '2020-01-01'},
+        description='出生年月日')
+    
+class PWInformationForm(InformationForm):
+    whether_password = BooleanField(
+        label='是否修改密码',
+        id="inputCheckbox")
+    password = PasswordField(
+        label='新密码',
+        validators=[
+            validators.Regexp(regex=r'^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,20}', message='请输入正确密码'),
+            validators.EqualTo('confirm', message='密码不匹配')
+        ],
+        description='8~16字母数字',
         id='inputPassword')
     confirm = PasswordField(
         label='确认密码',
+        description='再次输入密码',
         id='inputConfirm')
